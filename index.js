548a14ab9992a410ffe6adcecde4f98206b52130
cryarr <cryarr@oregonstate.edu>
2019-11-24 21:05:59 -0800

    refactored code now works with handlebars

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-	connectionLimit  : 10,
-	host			 : 'classmysql.engr.oregonstate.edu',
-	user 		     : 'cs340_cryarr',
-	password 		 : '3448',
-	database 		 : 'cs340_cryarr'
+var express = require('express');
+var mysql = require('./dbCon.js');
+const path = require('path');
+
+var app = express();
+
+var handlebars = require('express-handlebars').create({defaultLayout:'home'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('port', process.argv[2]);
+app.set('view engine', 'handlebars');
+
+app.set('myql', mysql);
+const publicPath = path.join(__dirname, '/vendor');
+app.use(express.static(publicPath));
+
+
+app.listen(app.get('port'), function() {
+	console.log('Express started on flip:' + app.get('port'));
 
 });
 
-module.exports.pool = pool;
+app.get('/', function(req, res, next){
+	res.render('landing');
+});
 
-function getRegion(res, mysql){
-        mysql.pool.query("SELECT * FROM region", function(err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-      }
-    )};
+app.get('/add', function(req, res, next){
+	res.render('add');
+});
+
+app.get('/addPerson', function(req, res, next){
+	res.render('addPerson');
+});
+
+app.get('/addRegion', function(req, res, next){
+	res.render('addRegion');
+});
+
+app.get('/addEvent', function(req, res, next){
+	res.render('addEvent');
+});
+
+app.get('/addCity', function(req, res, next){
+	res.render('addCity');
+});
+
+
+
+app.get('/search', function(req, res, next){
+	res.render('search');
+});
