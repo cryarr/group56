cd81e67e0232cb263868ae036eee35fca2d11c45
cryarr <cryarr@oregonstate.edu>
2019-11-25 11:59:18 -0800

    inserts now work

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var handlebars = require('express-handlebars').create({defaultLayout:'home'});
 app.engine('handlebars', handlebars.engine);
 app.set('port', process.argv[2]);
 app.set('view engine', 'handlebars');
-
+app.use(bodyparser.urlencoded({ extended: true }));
 app.set('myql', mysql);
 const publicPath = path.join(__dirname, '/vendor');
 app.use(express.static(publicPath));
@@ -93,7 +93,58 @@ app.get('/search', function(req, res, next){
 
 
 
+app.post('/addRegion', function(req, res, next){
+	var insert = "INSERT INTO region (region_name) VALUES ('" + req.body.rname + "')";
+	console.log(insert);
+	mysql.pool.query(insert, function(err, rows, fields){
+		if (err) {
+			console.log("error: " + err);
+		}
+		res.redirect('/addRegion');
+	});
+
+});
 
 
+app.post('/addPerson', function(req, res, next){
+	var insert = "INSERT INTO people (first_name,last_name,regid,citid,description) VALUES ('" +
+	req.body.fname + "','" +  req.body.lname + "','" + req.body.regid + "','" + req.body.citid + "','" + req.body.desc
+	+ "')";
+	console.log(insert);
+	mysql.pool.query(insert, function(err, rows, fields){
+		if (err) {
+			console.log("error: + " + err);
+		}
+		res.redirect('/addPerson');
+	});
+
+});
 
 
+app.post('/addEvent', function(req, res, next) {
+	var insert = "INSERT INTO event (event_name,description,reid,tpid) VALUES ('" +
+	req.body.ename + "','" + req.body.desc + "','" + req.body.reid + "','" + req.body.tpid + "')";
+	console.log(insert);
+	mysql.pool.query(insert, function(err, rows, fields){
+		if (err) {
+			console.log("error: + " + err);
+		}
+		res.redirect('/addEvent');
+	});
+
+});
+
+
+app.post('/addCity', function(req, res, next){
+	var insert = "INSERT INTO cities (city_name,population,rid) VALUES ('" + req.body.cname + 
+	"','" + req.body.pop + "','" + req.body.rid + "')";
+	console.log(insert);
+
+	mysql.pool.query(insert, function(err, rows, fields){
+		if (err) {
+			console.log("error: + " + err);
+		}
+		res.redirect('/addCity');
+	});
+
+});
