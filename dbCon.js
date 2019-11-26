b1fd0064a84e71b0f8f33d4a32d465850b0640c0
cryarr <cryarr@oregonstate.edu>
2019-11-25 20:41:14 -0800

    hiding my info

--- dbCon.js
+++ dbCon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
 	connectionLimit  : 10,
 	host			 : 'classmysql.engr.oregonstate.edu',
 	user 		     : 'cs340_cryarr',
-	password 		 : '3448',
+	password 		 : '***',
 	database 		 : 'cs340_cryarr'
 
 });
